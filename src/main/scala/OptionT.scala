2c7b47d9c2947c923d650d1ba68c3b2ff62e0b5e
Adrian Lopez <adrijardi@gmail.com>
2017-03-19 11:28:29 +0000

    This makes way more sense

--- src/main/scala/OptionT.scala
+++ src/main/scala/OptionT.scala
@@ -1,26 +1,16 @@
-import scala.collection.immutable.Seq
 import scala.language.higherKinds
 
-/**
-  * Created by adrij_000 on 13/03/2017.
-  */
 
 
 case class OptionL[A](e: Option[List[A]]) {
   def map[B](f: A => B): OptionL[B] = OptionL(e map(_.map(f)))
 
-  def flatMap[B](f: A => OptionL[B]): OptionL[B] = e match {
-    case None => OptionL(None)
-    case Some(list) =>
-      val transformed: Seq[Option[List[B]]] = list.map(b => f(b).e)
-      OptionL{
-        transformed.foldLeft(Option(List.empty[B])) {
-          case (Some(ret), Some(newVal)) => Some(ret ::: newVal)
-          case (_, _) => None
-        }
+  def flatMap[B](f: A => OptionL[B]): OptionL[B] =
+    OptionL {
+      e map {
+        _.flatMap(a => f(a).e.getOrElse(Nil))
       }
-  }
-
+    }
 }
 
 
@@ -28,20 +18,45 @@ case class OptionL[A](e: Option[List[A]]) {
 trait MonadicTransformer[T[_]] {
   def flatMap[A,B](monad: T[A], f: A => T[B]): T[B]
   def map[A,B](monad: T[A], f: A => B): T[B]
+  def pure[A](a: A): T[A]
 }
 object MonadicTransformer {
 
   implicit object ListMonadicTransformer extends MonadicTransformer[List] {
     override def flatMap[A,B](list: List[A], f: A => List[B]): List[B] = list.flatMap(f)
     override def map[A, B](list: List[A], f: (A) => B): List[B] = list.map(f)
+    override def pure[A](a: A): List[A] = List(a)
   }
 
   implicit object OptionMonadicTransformer extends MonadicTransformer[Option] {
     override def flatMap[A, B](option: Option[A], f: (A) => Option[B]): Option[B] = option.flatMap(f)
     override def map[A, B](option: Option[A], f: (A) => B): Option[B] = option.map(f)
+    override def pure[A](a: A): Option[A] = Some(a)
   }
 
 }
+
+case class OptionT[A, M[_]](m: M[Option[A]])(implicit mt: MonadicTransformer[M]) {
+
+  def flatMap[B](f: A => OptionT[B, M]): OptionT[B, M] = {
+    val f1: (Option[A]) => M[Option[B]] = _.map(a => f(a).m).getOrElse(mt.pure(None))
+    val map1: M[Option[B]] = mt.flatMap(m, f1)
+    OptionT.pure(map1)
+  }
+
+  def map[B](f: A => B): OptionT[B, M] = {
+    def f1(a:Option[A]): Option[B] = a.map(f)
+    val res: M[Option[B]] = mt.map(m, f1)
+    OptionT.pure(res)
+  }
+}
+
+object OptionT {
+  def pure[A, M[_]](m: M[Option[A]])(implicit mt: MonadicTransformer[M]): OptionT[A, M] = {
+    new OptionT(m)
+  }
+}
+
 
 //case class ListT[A, M[_]](m: M[List[A]])(implicit mt: MonadicTransformer[M]) {
 //  def flatMap[B](f: A => ListT[B, M]): ListT[B, M] = {
@@ -63,23 +78,3 @@ object MonadicTransformer {
 //}
 
 
-
-
-case class OptionT[A, M[_]](m: M[Option[A]])(implicit mt: MonadicTransformer[M]) {
-
-  def flatMap[B](f: A => OptionT[B, M]): OptionT[B, M] = {
-    OptionT.pure(mt.flatMap(m, (t: A) => f(t)))
-  }
-
-  def map[B](f: A => B): OptionT[B, M] = {
-    def f1(a:A): Option[B] = ???
-    val res: M[Option[B]] = mt.map(m, f1)
-    OptionT.pure(res)
-  }
-}
-
-object OptionT {
-  def pure[A, M[_]](m: M[Option[A]])(implicit mt: MonadicTransformer[M]): OptionT[A, M] = {
-    new OptionT(m)
-  }
-}
--- src/main/scala/test.sc
+++ src/main/scala/test.sc
@@ -5,6 +5,20 @@ val b = Some(List(2)): Option[List[Int]]
 
 OptionL(a).map(_+1)
 
+val la = List(Option(1), Option(5))
+val lb = List(Option(2), None)
+
+OptionT(la).map(_+1)
+OptionT(lb).flatMap(t => OptionT(List(Option(t+2))))
+
+val res = for {
+  a <- OptionT(la)
+  b <- OptionT(lb)
+} yield a + b
+
+res
+
+
 //ListT(a).map(_+1)
 //
 ////ListT(a).flatMap(b)
